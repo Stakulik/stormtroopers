bd02448b642300f78f80cbf5f642cdf5d4b3073d
Stakulik <stakulik@mail.ru>
2016-06-09 02:20:55 +0300

    Add seeds & gem for Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Person.create([
+  { "name": "Luke Skywalker",
+    "height": "172",
+    "mass": "77",
+    "hair_color": "blond",
+    "skin_color": "fair",
+    "eye_color": "blue",
+    "birth_year": "19BBY",
+    "gender": "male"},
+
+  { "name": "C-3PO",
+    "height": "167",
+    "mass": "75",
+    "hair_color": "n/a",
+    "skin_color": "gold",
+    "eye_color": "yellow",
+    "birth_year": "112BBY",
+    "gender": "n/a"}
+])
+
+Starship.create([
+  { "name": "CR90 corvette",
+    "model": "CR90 corvette"},
+  
+  { "name": "Star Destroyer",
+    "model": "Imperial I-class Star Destroyer"}
+])
