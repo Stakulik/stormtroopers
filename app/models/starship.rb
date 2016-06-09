2f8c64796d02a6194945dcb8e5e1f0d38d181f12
Stakulik <stakulik@mail.ru>
2016-06-09 17:52:37 +0300

    Fix pilots

--- app/models/starship.rb
+++ app/models/starship.rb
@@ -1,4 +1,4 @@
 class Starship < ApplicationRecord
-  has_and_belongs_to_many :pilots, class_name: "Person", foreign_key: :person_id
+  has_and_belongs_to_many :pilots, class_name: "Person", join_table: :pilots_starships, foreign_key: :person_id
   
 end
