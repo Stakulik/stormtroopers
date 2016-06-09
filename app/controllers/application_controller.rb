9bb1cf00c51f358fd9db90698da1e2b8b9c2d775
Stakulik <stakulik@mail.ru>
2016-06-09 13:06:47 +0300

    Try to fix allow-access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::API
   include ActionController::Serialization
+
+  before_action :add_allow_credentials_headers
   
+def add_allow_credentials_headers                                                                                                                                                       
+  response.headers["Access-Control-Allow-Origin"] = request.headers["Origin"] || "*"                                                                                                                                                                                                     
+  response.headers["Access-Control-Allow-Credentials"] = "true"                                                                                                                                                                                                                          
+end 
+
+def options                                                                                                                                                                                                                                                                              
+  head :status => 200, :"Access-Control-Allow-Headers" => "accept, content-type"                                                                                                                                                                                                         
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  match '*any' => 'application#options', :via => [:options]
 
   scope module: "api" do
     namespace :v1 do 
