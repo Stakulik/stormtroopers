a22cb2cc923adf2ec7dc880f391eaf0fb51e2fdd
Stakulik <stakulik@mail.ru>
2016-06-16 02:01:08 +0300

    Update tests

--- spec/requests/user_management_spec.rb
+++ spec/requests/user_management_spec.rb
@@ -1,12 +1,12 @@
 require "rails_helper"
 
-describe "User", type: :request do
+describe "User:", type: :request do
   let(:user) { create(:user) }
   let(:confirmed_user) { create(:user, :confirmed) }
   let(:headers_with_auth_token) { { "HTTP_ACCEPT": "application/json", "AUTHORIZATION": User.last.auth_token } }
 
-  describe "signup:" do
-    it "sends valid data, gets a success message and confirms an account" do
+  describe "signup" do
+    it "successfully" do
       post v1_signup_path, user: attributes_for(:user)
 
       expect(response.body).to include("Please go to your inbox #{User.last.email} and confirm creating an account")
@@ -16,7 +16,7 @@ describe "User", type: :request do
       expect(response.body).to include("Your account has been successfully confirmed")
     end
 
-    it "User sends invalid data and get errors" do
+    it "unsuccessfully (sends invalid data and gets errors)" do
       post v1_signup_path, user: attributes_for(:user, password: "pas", email: "" )
 
       expect(response.body).to include( "errors")
@@ -24,7 +24,7 @@ describe "User", type: :request do
   end
 
   describe "log in and log out" do
-    context "authenticated" do
+    context "with a confirmed account" do
       it "successfully" do
         log_in(confirmed_user)
 
@@ -32,11 +32,25 @@ describe "User", type: :request do
 
         expect(response.body).to include("Logged out successfully")
       end
+
+      it "unsuccessfully (gets errors due to invalid data)" do
+        post v1_login_path, { user: { email: confirmed_user.email, password: "wrongPassword" } }
+
+        expect(response.body).to include("That email/password combination is not valid")
+      end
     end
 
-    context "not authenticated" do
+    context "with an unconfirmed account" do
       it "unsuccessfully log in" do
-        post v1_login_path, {email: confirmed_user.email, password: "wrongPassword"}
+        post v1_login_path, { user: { email: user.email, password: user.password } }
+
+        expect(response.body).to include("You have to confirm your email")
+      end
+    end
+
+    context "without an account" do
+      it "unsuccessfully log in" do
+        post v1_login_path, { user: { email: "wrong@email.com", password: "wrongPassword" } }
 
         expect(response.body).to include("That email/password combination is not valid")
 
@@ -49,6 +63,124 @@ describe "User", type: :request do
         delete v1_logout_path, nil, { "HTTP_ACCEPT": "application/json" }
 
         expect(response.body).to include("Not Authenticated")
+      end
+    end
+  end
+
+  context "authenticated" do
+    describe "updates an account's" do
+      it "first name and last name successfully" do
+        log_in(confirmed_user)
+
+        put v1_user_path(confirmed_user), { user: { first_name: "Francisco", last_name: "D'anconia" } },
+          headers_with_auth_token
+
+        expect(response.body).to include("errors")
+
+        put v1_user_path(confirmed_user), { user: { first_name: "Francisco", last_name: "D'anconia",
+          current_password: confirmed_user.password } }, headers_with_auth_token
+
+        expect(response.body).to include("success")
+
+        get v1_user_path(confirmed_user), nil, headers_with_auth_token
+
+        expect(response.body).to include("Francisco")
+      end
+
+      it "password successfully" do
+        log_in(confirmed_user)
+
+        put v1_user_path(confirmed_user), { user: { password: "newpassword", password_confirmation: "newpassword" } },
+          headers_with_auth_token
+
+        expect(response.body).to include("errors")
+
+        put v1_user_path(confirmed_user), { user: { password: "newpassword", password_confirmation: "newpassword",
+          current_password: confirmed_user.password } }, headers_with_auth_token
+
+        expect(response.body).to include("success")
+
+        delete v1_logout_path, nil, headers_with_auth_token
+
+        expect(response.body).to include("Logged out successfully")
+
+        log_in(confirmed_user, nil, "newpassword")
+      end
+
+      it "email successfully" do
+        log_in(confirmed_user)
+
+        put v1_user_path(confirmed_user), { user: { email: "w@e", current_password: confirmed_user.password } }, headers_with_auth_token
+
+        expect(response.body).to include("errors")
+
+        put v1_user_path(confirmed_user), { user: { email: "new@example.com" } }, headers_with_auth_token
+
+        expect(response.body).to include("errors")
+
+        put v1_user_path(confirmed_user), { user: { email: "new@example.com", current_password: confirmed_user.password } },
+          headers_with_auth_token
+
+        expect(response.body).to include("success")
+
+        get v1_user_path(confirmed_user), nil, headers_with_auth_token
+
+        expect(response.body).to include("new@example.com")
+      end
+    end
+
+    describe "destroys an account" do
+      it "successfully" do
+        log_in(confirmed_user)
+
+        delete v1_destroy_account_path, nil, headers_with_auth_token
+
+        expect(response.body).to include("success")          
+      end
+    end
+
+    context "not authenticated" do
+      describe "updates an account's" do
+        it "first name and last name unsuccessfully" do
+          post v1_login_path, { user: { email: confirmed_user.email, password: "wrongPassword" } }
+
+          expect(response.body).to include("errors")
+
+          put v1_user_path(confirmed_user), { user: { first_name: "Francisco", last_name: "D'anconia",
+            current_password: confirmed_user.password } }, { "HTTP_ACCEPT": "application/json" }
+
+          expect(response.body).to include("Not Authenticated")
+        end
+
+        it "password unsuccessfully" do
+          post v1_login_path, { user: { email: confirmed_user.email, password: "wrongPassword" } }
+
+          expect(response.body).to include("errors")
+
+          put v1_user_path(confirmed_user), { user: { password: "newpassword", password_confirmation: "newpassword",
+            current_password: confirmed_user.password } }, { "HTTP_ACCEPT": "application/json" }
+
+          expect(response.body).to include("Not Authenticated")
+        end
+
+        it "email unsuccessfully" do
+          post v1_login_path, { user: { email: confirmed_user.email, password: "wrongPassword" } }
+
+          expect(response.body).to include("errors")
+
+          put v1_user_path(confirmed_user), { user: { email: "new@example.com", current_password: confirmed_user.password } },
+            { "HTTP_ACCEPT": "application/json" }
+
+          expect(response.body).to include("Not Authenticated")
+        end
+      end
+
+      describe "destroys an account" do
+        it "unsuccessfully" do
+          delete v1_destroy_account_path, nil, { "HTTP_ACCEPT": "application/json" }
+
+          expect(response.body).to include("Not Authenticated")      
+        end
       end
     end
   end
@@ -82,95 +214,12 @@ describe "User", type: :request do
       it "gets errors" do
         post v1_forgot_password_path, {email: user.email}
 
-        expect(response.body).to include("errors")
+        expect(response.body).to include("You have to confirm your email")
 
         post v1_update_password_path, { user: { email: user.email, password: "newpassword",
           password_confirmation: "newpassword" } }
 
-        expect(response.body).to include("errors")
-      end
-    end
-  end
-
-  describe "update an account" do
-    context "authenticated" do
-      it "updates a first name and a last name successfully" do
-        log_in(confirmed_user)
-
-        put v1_user_path(confirmed_user), { user: { first_name: "Francisco", last_name: "D'anconia" } },
-          headers_with_auth_token
-
-        expect(response.body).to include("errors")
-
-        put v1_user_path(confirmed_user), { user: { first_name: "Francisco", last_name: "D'anconia",
-          current_password: "qazwsx"} }, headers_with_auth_token
-
-        expect(response.body).to include("success")
-
-        get v1_user_path(confirmed_user), nil, headers_with_auth_token
-
-        expect(response.body).to include("Francisco")
-      end
-
-      it "updates a password successfully" do
-        log_in(confirmed_user)
-
-        put v1_user_path(confirmed_user), { user: { password: "newpassword", password_confirmation: "newpassword" } },
-          headers_with_auth_token
-
-        expect(response.body).to include("errors")
-
-        put v1_user_path(confirmed_user), { user: { password: "newpassword", password_confirmation: "newpassword",
-          current_password: "qazwsx"} }, headers_with_auth_token
-
-        expect(response.body).to include("success")
-
-        delete v1_logout_path, nil, headers_with_auth_token
-
-        log_in(confirmed_user, nil, "newpassword")
-      end
-
-      it "updates an email successfully" do
-        log_in(confirmed_user)
-
-        put v1_user_path(confirmed_user), { user: { email: "w@e", current_password: "qazwsx" } }, headers_with_auth_token
-
-        expect(response.body).to include("errors")
-
-        put v1_user_path(confirmed_user), { user: { email: "new@example.com" } }, headers_with_auth_token
-
-        expect(response.body).to include("errors")
-
-        put v1_user_path(confirmed_user), { user: { email: "new@example.com", current_password: "qazwsx"} },
-          headers_with_auth_token
-
-        expect(response.body).to include("success")
-
-        get v1_user_path(confirmed_user), nil, headers_with_auth_token
-
-        expect(response.body).to include(User.last.email)
-      end
-    end
-  end
-
-  describe "destroy an account" do
-    context "authenticated" do
-      it "successfully" do
-        log_in(confirmed_user)
-
-        delete v1_destroy_account_path, nil, headers_with_auth_token
-
-        expect(response.body).to include("success")          
-      end
-    end
-
-    context "not authenticated" do
-      it "gets errors" do
-        log_in(confirmed_user)
-
-        delete v1_destroy_account_path, nil, { "HTTP_ACCEPT": "application/json" }
-
-        expect(response.body).to include("errors")          
+        expect(response.body).to include("You have to confirm your email")
       end
     end
   end
--- spec/support/user_session_helper.rb
+++ spec/support/user_session_helper.rb
@@ -1,7 +1,7 @@
 module UserSessionHelper
 
   def log_in(user, email = nil, password = nil)
-    post v1_login_path, { email: email || user.email, password: password || user.password }
+    post v1_login_path, { user: { email: email || user.email, password: password || user.password } }
 
     expect(response.body).to include("auth_token")
 
