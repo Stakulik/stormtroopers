db5e1c5710c4f9eefa2f494d99944feeded7f04c
Stakulik <stakulik@mail.ru>
2016-07-04 23:52:44 +0300

    Update sw units serializers

--- app/serializers/sw_units/index_serializer.rb
+++ app/serializers/sw_units/index_serializer.rb
@@ -1,3 +1,17 @@
 class SwUnits::IndexSerializer < SwUnitSerializer
-  attributes :id, :name
+
+  def attributes
+    filter_main_params(object)
+  end
+
+  def filter_main_params(object)
+    params =
+      if object.class.to_s == "Person"
+        %w(id name birth_year)
+      else
+        %w(id name)
+      end
+
+    object.attributes.select { |key, v| params.include?(key) }
+  end
 end
--- app/serializers/sw_units/show_serializer.rb
+++ app/serializers/sw_units/show_serializer.rb
@@ -1,3 +1,47 @@
 class SwUnits::ShowSerializer < SwUnitSerializer
-  attributes :id, :name
+  def attributes
+    filtered_object = filter_additional_params(object)
+
+    add_related_objects(filtered_object)
+  end
+
+  def filter_additional_params(object)
+    params = define_additional_params(object)
+
+    object.attributes.select { |key, v| params.include?(key) }
+  end
+
+  def add_related_objects(filtered_object)
+    related_objects = get_related_objects(filtered_object)
+
+    filtered_object["#{related_objects[0..-2]}_ids"] = object.send(related_objects).map(&:id)
+
+    filtered_object[related_objects] = object.send(related_objects).map do |ro|
+      filter_additional_params(ro)
+    end
+
+    filtered_object
+  end
+
+  def define_additional_params(object)
+    if object.class.to_s == "Person"
+      %w(id name birth_year eye_color gender hair_color height mass skin_color)
+    elsif object.class.to_s == "Planet"
+      %w(id name rotation_period orbital_period climate diameter gravity surface_water terrain
+        population)
+    else
+      %w(id name model manufacturer cost_in_credits max_atmosphering_speed passengers
+      cargo_capacity consumables hyperdrive_rating MGLT starship_class length crew)
+    end
+  end
+
+  def get_related_objects(filtered_object)
+    if filtered_object.key?("gender")
+      "starships"
+    elsif filtered_object.key?("gravity")
+      "residents"
+    else
+      "pilots"
+    end
+  end
 end
