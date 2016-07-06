b6dd0ebf9bfaef9048a26d04a5aebe21941b652d
Stakulik <stakulik@mail.ru>
2016-07-06 14:29:42 +0300

    Change birth_year type

---
+++ db/migrate/20160706111421_change_people_birth_year.rb
@@ -0,0 +1,22 @@
+class ChangePeopleBirthYear < ActiveRecord::Migration[5.0]
+  def change
+    execute <<-SQL
+      create or replace function cast_to_int(text, integer) returns integer as $$
+      begin
+        return cast($1 as integer);
+      exception
+        when invalid_text_representation then
+          return $2;
+      end;
+      $$ language plpgsql immutable;
+    SQL
+
+    execute <<-SQL
+      alter table people alter birth_year drop default;
+
+      alter table people alter column birth_year type integer using cast_to_int(#{birth_year[0..-4]}, 0);
+
+      alter table people alter birth_year set default 0;
+    SQL
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160701083626) do
+ActiveRecord::Schema.define(version: 20160706111421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +25,7 @@ ActiveRecord::Schema.define(version: 20160701083626) do
 
   create_table "people", force: :cascade do |t|
     t.string   "name",       default: "", null: false
-    t.string   "birth_year", default: "", null: false
+    t.integer  "birth_year", default: 0,  null: false
     t.string   "eye_color",  default: "", null: false
     t.string   "gender",     default: "", null: false
     t.string   "hair_color", default: "", null: false
