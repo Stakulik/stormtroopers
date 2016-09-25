318c27703abf5637815d4d2205e12d18fb3e1d8f
Stakulik <stakulik@mail.ru>
2016-09-25 15:28:23 +0300

    Add Figaro and update env variables

--- Gemfile
+++ Gemfile
@@ -20,7 +20,9 @@ gem "haml-rails", "~> 0.9.0"
 
 gem "kaminari"
 
-gem 'delayed_job_active_record'
+gem "delayed_job_active_record"
+
+gem "figaro"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/environment.rb
+++ config/environment.rb
@@ -7,9 +7,11 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   user_name: ENV["SG_USER_NAME"],
   password: ENV["SG_PASSWORD"],
-  domain: "stormtroopers.herokuapp.com",
+  domain: ENV["BACKEND_DOMAIN"],
   address: "smtp.sendgrid.net",
   port: 587,
   authentication: :plain,
   enable_starttls_auto: true
 }
+
+ActionMailer::Base.delivery_method = :smtp
